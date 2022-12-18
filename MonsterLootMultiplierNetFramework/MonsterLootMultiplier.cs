c2816bae0264f3d407ed26530c7985be5b2609ea
A.Dude.Programmer <A.Dude.Programmer@gmail.com>
2022-12-18 10:26:57 -0700

    Initial commit

---
+++ MonsterLootMultiplierNetFramework/MonsterLootMultiplier.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.IO;
+using System.Reflection;
+
+namespace MonsterLootMultiplierMod
+{
+    [BepInPlugin("MonsterLootMultiplier", "MonsterLootMultiplier", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class MonsterLootMultiplier : BaseUnityPlugin
+    {
+        public const string MODNAME = "MonsterLootMultiplier";
+
+        public const string AUTHOR = "sodenke";
+
+        public const string GUID = "MonsterLootMultiplier";
+
+        public const string VERSION = "1.0.0";
+
+        internal readonly ManualLogSource log;
+
+        internal readonly Harmony harmony;
+
+        internal readonly Assembly assembly;
+
+        public readonly string modFolder;
+
+        public static ConfigEntry<int> lootMultiplier;
+
+        public MonsterLootMultiplier()
+        {
+            log = base.Logger;
+            harmony = new Harmony("MonsterLootMultiplier");
+            assembly = Assembly.GetExecutingAssembly();
+            modFolder = Path.GetDirectoryName(assembly.Location);
+            Harmony.CreateAndPatchAll(typeof(MonsterLootMultiplierMod.Patches));
+        }
+
+        public void Awake()
+        {
+            lootMultiplier = base.Config.Bind("General", "Multiplier for monster drops", 1, " Monster Drop Multiplier");
+        }
+    }
+}
---
+++ MonsterLootMultiplierNetFramework/Patches.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace MonsterLootMultiplierMod
+{
+    public class Patches
+    {
+        [HarmonyPatch(typeof(CharacterDrop), "GenerateDropList")]
+        [HarmonyPrefix]
+        private static bool LootMultiplierCustom(CharacterDrop __instance, ref List<KeyValuePair<GameObject, int>> __result)
+        {
+            List<KeyValuePair<GameObject, int>> list = new List<KeyValuePair<GameObject, int>>();
+            // m_character is no longer public so no level multiplier for this mod.
+            //int num = ((!__instance.m_character) ? 1 : Mathf.Max(1, (int)Mathf.Pow(2f, __instance.m_character.GetLevel() - 1)));
+            int num = 1;
+            foreach (CharacterDrop.Drop drop in __instance.m_drops)
+            {
+                if (drop.m_prefab == null)
+                {
+                    continue;
+                }
+                float num2 = drop.m_chance;
+                if (drop.m_levelMultiplier)
+                {
+                    num2 *= (float)num;
+                }
+                if ((Random.value <= num2))
+                {
+                    int num3 = Random.Range(drop.m_amountMin, drop.m_amountMax);
+                    if (drop.m_levelMultiplier)
+                    {
+                        num3 *= num;
+                    }
+                    if (drop.m_onePerPlayer)
+                    {
+                        num3 = ZNet.instance.GetNrOfPlayers();
+                    }
+                    if (num3 > 0)
+                    {
+                        list.Add(new KeyValuePair<GameObject, int>(drop.m_prefab, num3 * MonsterLootMultiplier.lootMultiplier.Value));
+                    }
+                }
+            }
+            __result = list;
+            return false;
+        }
+
+        [HarmonyPatch(typeof(Fish), "RPC_Pickup")]
+        [HarmonyPrefix]
+        private static bool FishStack(Fish __instance)
+        {
+            __instance.m_pickupItemStackSize *= MonsterLootMultiplier.lootMultiplier.Value;
+            return true;
+        }
+    }
+}
